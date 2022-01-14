5e0b65a029c4ae1552615db87d880086c55dfe0f
jimrox-console <jimroxodezi@gmail.com>
2022-01-14 20:34:04 +0100

    update matrix class

--- src/mthlib/matrix.py
+++ src/mthlib/matrix.py
@@ -1,4 +1,38 @@
+#from array import array
+
+
+
 
 class Matrix(object):
-    def __init__(self):
-        ...
+    
+    def __init__(self, rows: int, columns: int):
+        self._rows = rows
+        self._columns = columns
+        self._matrix = []
+
+        for i in range(rows):
+            self._matrix.append([]) 
+
+        for row in self._matrix:
+            for i in range(columns):
+                row.append(0) 
+
+    def __setitem__(self, key, value):
+        if isinstance(value, list):
+            self.matrix[key] = value
+        else:
+            raise TypeError("A matrix object can only contain lists of numbers")
+        return
+
+    def __repr__(self):
+        '''Print the matrix row after row.'''
+        prt = ""
+        for row in self._matrix:
+            prt += str(row)
+            prt += "\n"
+        return prt.rstrip()
+
+
+
+mat = Matrix(3, 3)
+print(mat)
