6960e78fb20c941e0338b65a1d57ce3a11bfdb87
jimrox-console <jimroxodezi@gmail.com>
2022-01-14 20:34:21 +0100

    update polynomial class

--- src/mthlib/polynomial.py
+++ src/mthlib/polynomial.py
@@ -1,4 +1,13 @@
 
-class Polynomial(object):
 
-    def __init__(self):...
+
+
+class Polynomial:
+    def __init__(self, *coefficients):
+        """ input: coefficients are in the form a_n, ...a_1, a_0"""
+    self.coefficients = list(coefficients)
+
+    def __repr__(self):
+        """method to return the canonical string representation
+        of a polynomial."""
+        return "Polynomial" + str(tuple(self.coefficients))
