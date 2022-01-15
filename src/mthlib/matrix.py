d2f3ed5f10e9640245f5a1c6b3e9ae3f5e6e13bc
jimrox-console <jimroxodezi@gmail.com>
2022-01-15 16:36:12 +0100

    update matrix class

--- src/mthlib/matrix.py
+++ src/mthlib/matrix.py
@@ -2,27 +2,28 @@
 #TODO: class is not generic. only works with int. Make class work with all numerical types
 #TODO: add type annotations to every bit of this code
 from numbers import Number
+from random import randrange, randint, random
 
 
 class Matrix(object):
     
-    def __init__(self, rows_len: int, columns_len: int, fill_value: Number = 0):
+    def __init__(self, rows: int, columns: int, fill_value: Number = 0):
         
         #error-check for size of matrix. Size must be integers
         if isinstance(rows, int) and isinstance(columns, int):
-            self._rows = rows_len
-            self._columns = columns_len
+            self._rows = rows
+            self._columns = columns
         else:
             raise TypeError("Matrix row and column length must be integers!")
 
         if isinstance(fill_value, Number):
-            self._matrix = [[fill_value] * columns for _ in range(rows)]
+            self._matrix = [[fill_value] * self._columns for _ in range(self._rows)]
         else:
             raise TypeError("Matrix elements must be of numeric type!")
        
 
     def __setitem__(self, idx: int, value: list):
-        if isinstance(value, list):
+        if isinstance(value, list) and isinstance(idx, int):
             self._matrix[idx] = value
         else:
             raise TypeError("A matrix object can only contain lists of numbers")
@@ -68,6 +69,15 @@ class Matrix(object):
         raise NotImplementedError
 
 
+    # def identity(n):
+    #     """
+    #     Creates a n x n identity matrix.
+    #     """
+    #     I = zeroes(n, n)
+    #     for i in range(n):
+    #         I.g[i][i] = 1.0
+    #     return I
+
     # @classmethod
     # def identity(cls, m):
     #     """ Make identity matrix of rank (mxm) """
@@ -82,6 +92,17 @@ class Matrix(object):
     #     return cls.fromList(rows)
 
 
+
+    @classmethod
+    def randomMat(cls,row_size, column_size, lower=-5, upper=5):
+        '''Fill the matrix with random numbers (integers).'''
+        newMatrix = Matrix(row_size, column_size)    
+        for row in newMatrix:
+            for i in range(newMatrix._columns):
+                row[i] = randint(lower, upper)
+        return newMatrix
+
+
 if __name__ == "__main__":
         
     mat = Matrix(3, 3)
@@ -92,3 +113,5 @@ if __name__ == "__main__":
     mat[0][1] = 10
 
     print(mat)
+    mat1 = Matrix.randomMat(3, 3, 100, 200)
+    print(mat1)
